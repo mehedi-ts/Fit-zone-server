27d370e66854dac81d46322813cb5184d9e028cc
mehedi hasan <mehedihasan.j135@gmail.com>
2026-06-19 23:29:14 +0600

    feat: integrate MongoDB for data storage and add API endpoint for classes

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
+
+const client = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    //database collections are created here
+    const db = client.db("fitzone");
+    const usersCollection = db.collection("user");
+    const classesCollection = db.collection("classes");
+
+    //database collections are ended here
+
+    //api routes are created here
+    app.post("/api/classes", async (req, res) => {
+      const newClass = req.body;
+      const result = await classesCollection.insertOne(newClass);
+      res.send(result);
+    });
+
+    //api routes are ended here
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Fit Zone Server is running");
